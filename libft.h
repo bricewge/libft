57a967fb63e26400a03f0697820166a2b9934267
Brice Waegeneire <brice.wge@gmail.com>
2016-12-21 17:12:35 +0100

    add ft_strrchr

--- libft.h
+++ libft.h
@@ -30,6 +30,7 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
 char	*ft_strcat(char *dest, char *src);
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
