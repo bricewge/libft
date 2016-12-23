ad15322b8234c9c97447190fb428f949c6f4433d
Brice Waegeneire <brice.wge@gmail.com>
2016-12-23 15:11:01 +0100

    add ft_strncat and use it in ft_strcat

--- ft_strncat.c
+++ ft_strncat.c
@@ -1,18 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bwaegene <bwaegene@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/12/19 14:58:19 by bwaegene          #+#    #+#             */
-/*   Updated: 2016/12/23 15:08:39 by bwaegene         ###   ########.fr       */
+/*   Created: 2016/12/23 14:06:35 by bwaegene          #+#    #+#             */
+/*   Updated: 2016/12/23 15:05:46 by bwaegene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strcat(char *s1, const char *s2)
+char	*ft_strncat(char *s1, const char *s2, size_t n)
 {
-	return (ft_strncat(s1, s2, ft_strlen((char *)s2)));
+	size_t		len1;
+	size_t		len2;
+
+	len1 = ft_strlen(s1);
+	len2 = 0;
+	while (s2[len2] && len2 < n)
+	{
+		s1[len1 + len2] = s2[len2];
+		len2++;
+	}
+	s1[len1 + len2] = '\0';
+	return (s1);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: bwaegene <bwaegene@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/16 20:40:18 by bwaegene          #+#    #+#             */
-/*   Updated: 2016/12/23 11:32:42 by bwaegene         ###   ########.fr       */
+/*   Updated: 2016/12/23 15:04:21 by bwaegene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,7 @@ char	*ft_strncpy(char *dst, const char *src, size_t len);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strcat(char *s1, const char *s2);
+char	*ft_strncat(char *s1, const char *s2, size_t n);
 char	*ft_strstr(const char *big, const char *little);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 int		ft_strcmp(char *s1, char *s2);
