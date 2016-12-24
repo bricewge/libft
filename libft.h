dabb1e48ee914fe0a8bbce470f555a182683c547
Brice Waegeneire <brice.wge@gmail.com>
2016-12-24 17:54:33 +0100

    add ft_memalloc

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: bwaegene <bwaegene@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/16 20:40:18 by bwaegene          #+#    #+#             */
-/*   Updated: 2016/12/24 16:57:29 by bwaegene         ###   ########.fr       */
+/*   Updated: 2016/12/24 17:43:03 by bwaegene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,6 +49,7 @@ int		ft_tolower(int c);
 /*
 ** Second part
 */
+void	*ft_memalloc(size_t size);
 int		ft_atoi(const char *str);
 void	ft_putstr(char *str);
 void	ft_putnbr(int nb);
