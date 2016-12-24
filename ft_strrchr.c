1c199b09b22adbc5cd74a1b5f52713b79bc586fd
Brice Waegeneire <brice.wge@gmail.com>
2016-12-24 17:29:34 +0100

    fix ft_strrchr
    It did not searched the first char of the string

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: bwaegene <bwaegene@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/21 16:53:13 by bwaegene          #+#    #+#             */
-/*   Updated: 2016/12/21 17:07:58 by bwaegene         ###   ########.fr       */
+/*   Updated: 2016/12/24 17:28:34 by bwaegene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*ft_strrchr(const char *s, int c)
 	int len;
 
 	len = ft_strlen((char *)s);
-	while (len)
+	while (len >= 0)
 	{
 		if (s[len] == (char)c)
 			return (&((char *)s)[len]);
