c1e7ba6abde4811616316c50fb26e9e7243d09cb
Brice Waegeneire <brice.wge@gmail.com>
2016-12-26 16:34:17 +0100

    add ft_putnbr_fd

--- libft.h
+++ libft.h
@@ -69,6 +69,7 @@ void	ft_putendl(char const *s);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char const *s, int fd);
 void	ft_putendl_fd(char const *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 char	*ft_itoa(int nb);
 void	ft_putchar(int c);
 /*
