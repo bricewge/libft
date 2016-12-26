8f3b90a8a353ee215a4211ca340df931efa4726e
Brice Waegeneire <brice.wge@gmail.com>
2016-12-26 11:02:25 +0100

    ft_memalloc malloc by the size of the content not the address size

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -6,7 +6,7 @@
 /*   By: bwaegene <bwaegene@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/24 17:34:04 by bwaegene          #+#    #+#             */
-/*   Updated: 2016/12/24 17:53:43 by bwaegene         ###   ########.fr       */
+/*   Updated: 2016/12/26 11:00:59 by bwaegene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	*ft_memalloc(size_t size)
 {
 	void	*result;
 
-	result = (void *)malloc(sizeof(void *) * size);
+	result = (void *)malloc(sizeof(*result) * size);
 	if (result)
 	{
 		ft_memset(result, 0, size);
