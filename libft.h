338fcacebd48a58fc999637a267ab268be6885f7
Brice Waegeneire <brice.wge@gmail.com>
2016-12-28 17:32:31 +0100

    add ft_lstadd

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: bwaegene <bwaegene@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/16 20:40:18 by bwaegene          #+#    #+#             */
-/*   Updated: 2016/12/28 16:04:14 by bwaegene         ###   ########.fr       */
+/*   Updated: 2016/12/28 16:08:22 by bwaegene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,7 +70,7 @@ char					*ft_strmapi(char const *s,
 int						ft_strequ(char const *s1, char const *s2);
 int						ft_strnequ(char const *s1, char const *s2, size_t n);
 char					*ft_strsub(char const *s, unsigned int start,
-								size_t len);
+									size_t len);
 char					*ft_strjoin(char const *s1, char const *s2);
 char					*ft_strtrim(char const *s);
 char					**ft_strsplit(char const *s, char c);
@@ -91,6 +91,7 @@ t_list					*ft_lstnew(void const *content, size_t content_size);
 void					ft_lstdelone(t_list **alst,
 									void (*del)(void *, size_t));
 void					ft_lstdel(t_list **alst, void (*del)(void *, size_t));
+void					ft_lstadd(t_list **alst, t_list *new);
 /*
 ** Additional part
 */
