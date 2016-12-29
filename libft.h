696d6b4c727775975f13303a54844499b7b3626a
Brice Waegeneire <brice.wge@gmail.com>
2016-12-29 05:05:00 +0100

    add ft_lstmap

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: bwaegene <bwaegene@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/16 20:40:18 by bwaegene          #+#    #+#             */
-/*   Updated: 2016/12/28 17:33:42 by bwaegene         ###   ########.fr       */
+/*   Updated: 2016/12/28 18:06:55 by bwaegene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -93,6 +93,7 @@ void					ft_lstdelone(t_list **alst,
 void					ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void					ft_lstadd(t_list **alst, t_list *new);
 void					ft_lstiter(t_list *lst, void (*f)(t_list *elem));
+t_list					*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 /*
 ** Additional part
 */
