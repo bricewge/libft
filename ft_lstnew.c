e89435d26aed237b5bd707831699e021b84cc321
Brice Waegeneire <brice.wge@gmail.com>
2016-12-29 05:05:24 +0100

    cast malloc in ft_lstnew

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: bwaegene <bwaegene@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/28 14:04:59 by bwaegene          #+#    #+#             */
-/*   Updated: 2016/12/28 14:48:02 by bwaegene         ###   ########.fr       */
+/*   Updated: 2016/12/28 19:22:43 by bwaegene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ t_list	*ft_lstnew(void const *content, size_t content_size)
 {
 	t_list	*new;
 
-	new = malloc(sizeof(t_list));
+	new = (t_list *)malloc(sizeof(*new));
 	if (new)
 	{
 		if (!content)
